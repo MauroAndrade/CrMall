489a305a9b3a41cdc6a4296bae20bc5a93eb461c
Mauro Fernando Thomaz de Andrade <nocnoier@hotmail.com>
2021-01-22 07:27:51 -0300

    Subindo o Projeto

---
+++ Data/Models/Database/crmallContext.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Data.Models.Database
+{
+    public partial class crmallContext : DbContext
+    {
+        public crmallContext()
+        {
+        }
+
+        public crmallContext(DbContextOptions<crmallContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Clientes> Clientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=127.0.0.1;port=3306;user=root;password=123456;database=crmall", x => x.ServerVersion("8.0.21-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Clientes>(entity =>
+            {
+                entity.ToTable("clientes");
+
+                entity.HasIndex(e => e.Id)
+                    .HasName("id_UNIQUE")
+                    .IsUnique();
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Bairro)
+                    .HasColumnName("bairro")
+                    .HasColumnType("varchar(150)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.Property(e => e.Cep)
+                    .HasColumnName("cep")
+                    .HasColumnType("varchar(9)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.Property(e => e.Cidade)
+                    .HasColumnName("cidade")
+                    .HasColumnType("varchar(150)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.Property(e => e.Complemento)
+                    .HasColumnName("complemento")
+                    .HasColumnType("varchar(100)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.Property(e => e.Endereco)
+                    .HasColumnName("endereco")
+                    .HasColumnType("varchar(150)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.Property(e => e.Estado)
+                    .HasColumnName("estado")
+                    .HasColumnType("varchar(150)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.Property(e => e.Nascimento)
+                    .HasColumnName("nascimento")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasColumnName("nome")
+                    .HasColumnType("varchar(150)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.Property(e => e.Numero)
+                    .HasColumnName("numero")
+                    .HasColumnType("varchar(10)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.Property(e => e.Sexo)
+                    .IsRequired()
+                    .HasColumnName("sexo")
+                    .HasColumnType("varchar(10)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
